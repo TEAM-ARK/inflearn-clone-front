28d8205f1b8de360029ff7cb6e160cc0ccb6c1a6
TaehwanGo <gth1123@naver.com>
2021-07-25 00:05:17 +0900

    chore : fix typo

--- src/components/Slider/MainSliderItem.tsx
+++ src/components/Slider/MainSliderItem.tsx
@@ -14,59 +14,56 @@ interface IMainSliderItemProps {
   };
 }
 
-const MainSliderItem = ({ key, item }: Iitem) => {
+const ItemWrapper = styled.div`
+  display: flex !important;
+  height: 320px;
+  max-width: 1200px;
+  margin: 0 10px;
+  margin: auto;
+`;
+
+const ItemLeft = styled.div`
+  background: gray;
+  width: 45%;
+  height: 100%;
+  padding: 30px 10px;
+  .pre {
+    white-space: pre;
+    word-break: keep-all;
+  }
+
+  .item__tags {
+    background-color: red;
+    display: inline-block;
+    padding: 5px;
+    border-radius: 4px;
+    font-size: 0.9rem;
+    margin-bottom: 0.9rem;
+  }
+  .item__title {
+    font-size: 26px;
+  }
+  .item__text {
+    font-weight: 600;
+    color: whitesmoke;
+  }
+`;
+
+const ItemRight = styled.div`
+  width: 55%;
+  height: 100%;
+
+  & > img {
+    background: center/cover;
+    max-width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: left;
+  }
+`;
+
+const MainSliderItem = ({ key, item }: IMainSliderItemProps) => {
   const [isMobile, setIsMobile] = useState(false);
-
-  const ItemWrapper = styled.div`
-    display: flex !important;
-    height: 320px;
-
-    max-width: 1200px;
-    margin: 0 10px;
-    margin: auto;
-  `;
-
-  const ItemLeft = styled.div`
-    background: gray;
-    width: 45%;
-    height: 100%;
-    padding: 30px 10px;
-    .pre {
-      white-space: pre;
-      word-break: keep-all;
-    }
-
-    .item__tags {
-      background-color: red;
-      display: inline-block;
-      padding: 5px;
-      border-radius: 4px;
-      font-size: 0.9rem;
-      margin-bottom: 0.9rem;
-    }
-    .item__title {
-      font-size: 26px;
-    }
-    .item__text {
-      font-weight: 600;
-      color: whitesmoke;
-    }
-  `;
-
-  const ItemRight = styled.div`
-    width: 55%;
-    height: 100%;
-
-    & img {
-      background: center/cover;
-      src: ${isMobile ? item?.mobileImg : item.pcImg};
-      alt: ${item.title};
-      max-width: 100%;
-      height: 100%;
-      object-fit: cover;
-      object-position: left;
-    }
-  `;
 
   const handleResize = () => {
     if (window.innerWidth < 768) {
@@ -85,7 +82,7 @@ const MainSliderItem = ({ key, item }: Iitem) => {
   });
 
   return (
-    <ItemWrapper key={item.id}>
+    <ItemWrapper key={key}>
       <ItemLeft>
         {item.tags[0] && <div className="item__tags item__text">{item.tags.map((tag) => tag)}</div>}
         {/* <div className="item__tags item__text">{item.tags.map(tag => tag)}</div> */}
