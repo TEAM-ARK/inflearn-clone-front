7592813e0b6dae85bb87d3a626d11c0b6ecadc3a
TaehwanGo <gth1123@naver.com>
2021-08-04 00:39:22 +0900

    setting redux

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -21,6 +21,10 @@ export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
 const dummyUser = (data) => ({
   ...data,
   nickname: 'Ark',
