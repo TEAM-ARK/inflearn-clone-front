aa4412393d11d66064821bc2995b88c1e953a653
hyoni0817 <dudu1104@naver.com>
2021-09-30 15:21:48 +0900

    강의 리스트 스타일 선택 버튼 구현 (#77)
    * Feat: 강의 정렬 스타일을 선택하는 Gird와 List 버튼 구현
    
    * Feat: 강의 리스트 불러오는 것 구현
    
    * Feat: ListViewBtn 클릭시 Grid나 List 스타일로 표현되게 구현
    
    - 어떤 버튼을 선택했는지 표시되게 함.
    
    - 스타일 변경 버튼을 클릭했을 때, 변경된 스타일 상태값이 URL 쿼리 스트링의 view 파라미터에도 전달되게 함.
    
    * Add: HorizonLectureCard 컴포넌트 추가 및 컨텐츠 일부 디자인
    
    - 리스트 스타일 강의 카드 레이아웃 구현 완료함.
    
    - 썸네일, 타이틀, 가격, 아이콘 버튼 부분은 반응형 까지 고려하여 디자인 완료함.
    
    * Add: LecturePrice 컴포넌트를 생성하여 가격 표시
    
    - Grid 스타일에서도 사용할 수 있게끔 만듦.
    
    * Fix: 할인된 가격이 나타나게끔 수정
    
    * Add: 강의 코멘트 갯수의 위치 및 폰트 크기 수정
    
    * Add: 해시태그 디자인
    
    * Fix: LectureShoppingContents 오타 수정
    
    * Add: 선택된 강의 정렬 스타일 버튼에 hover 효과
    
    * Add: 강의 리스트를 나타내는 부분의 padding값을 너비에 따라 다르게 부여
    
    * chore: 강의 페이지 경로
    
    * Fix: props 이름에 있는 대문자로 인해 warning이 발생해서 이름을 수정
    
    * Fix: hover시 컴포넌트 리렌더링 문제로 인해 아이콘 버튼들을 컴포넌트로 따로 분리
    
    -  hover 관련 state가 변경될 때마다 HorizonLectureCard가 전체 리렌더링 되는 문제가 발생함.
    
    - hover 관련 state를 사용하는 아이콘 버튼들을 IconButtons 컴포넌트로 분리시켜 문제 해결함.
    
    * Update: 강의 카드 스타일 선택 버튼 구현 관련 작업 내역 및 앞으로 진행할 작업
    
    * Fix: map을 사용한 해시 태그 부분에 key 추가
    
    - Warning: Each child in a list should have a unique "key" prop. 경고 해결함.
    
    * Fix: HorizonLectureCard 오타 수정

---
+++ src/components/lectureCard/HorizonLectureCard.tsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import styled from 'styled-components';
+import IconButtons from '@components/lectureCard/IconButtons';
+import LecturePrice from '@components/lectureCard/LecturePrice';
+import RatingStar from '@components/lectureCard/RatingStar';
+import { ILecture } from 'src/redux/reducers/types';
+
+type HorizonLectureCardStyleProps = {
+  index: number;
+};
+
+const HorizonLectureCardStyle = styled.li<HorizonLectureCardStyleProps>`
+  display: flex;
+  padding: 1.25rem;
+  border-top: ${(props) => (props.index > 0 ? '1px solid hsla(0, 0%, 85.9%, 0.5);' : 'none')};
+
+  @media screen and (max-width: 768px) {
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const LectureCardContents = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const LectureCardImage = styled.img`
+  width: 8rem;
+  margin-right: 1rem;
+
+  @media screen and (max-width: 768px) {
+    width: 4rem;
+  }
+`;
+
+const LectureTitle = styled.h2`
+  @media screen and (max-width: 768px) {
+    font-size: 0.85rem;
+  }
+`;
+
+const HashTagsWrapper = styled.div`
+  margin: 0.25rem 0;
+  display: flex;
+`;
+type HashTagProps = {
+  randomColor: () => string;
+};
+
+const HashTagStyle = styled.p<HashTagProps>`
+  display: inline-block;
+  color: #454545;
+  background-color: ${(props) => props.randomColor};
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  padding: 0 6px;
+  font-size: 0.7rem;
+  font-weight: 500;
+  border-radius: 10px;
+`;
+
+const LectureShoppingContents = styled.div`
+  margin-left: auto;
+  text-align: right;
+
+  @media screen and (max-width: 768px) {
+    position: relative;
+  }
+`;
+
+const LecturePriceWrapper = styled.div`
+  @media screen and (max-width: 768px) {
+    & > * {
+      display: block;
+    }
+  }
+`;
+
+const LectureCardIconBtnWrapper = styled.div`
+  display: inline-block;
+
+  @media screen and (max-width: 768px) {
+    padding-right: 20px;
+  }
+`;
+
+const LectureCommentCount = styled.span`
+  position: absolute;
+  font-size: 0.7rem;
+`;
+
+type Props = {
+  lecture: ILecture;
+  index: number;
+};
+
+const HorizonLectureCard = ({ lecture, index }: Props) => {
+  const { id, coverImage, title, rating, commentCount, price, hashTags, onDiscount } = lecture;
+
+  const getRandomColor = () => {
+    const h = 360 * Math.random();
+    const s = 25 + 70 * Math.random();
+    const l = 85 + 10 * Math.random();
+
+    return `hsl(${h} ,${s}%, ${l}%)`;
+  };
+
+  return (
+    <HorizonLectureCardStyle key={id} index={index}>
+      <a id="thumbnail" href={`/course/${id}`}>
+        <LectureCardImage loading="lazy" src={coverImage} alt={title} />
+      </a>
+      <LectureCardContents>
+        <a id="lecture-info-contnents" href={`/course/${id}`}>
+          <LectureTitle>{title}</LectureTitle>
+          <HashTagsWrapper>
+            {!!hashTags &&
+              hashTags.map((val: string, idx: number) => (
+                <HashTagStyle key={idx} randomColor={getRandomColor}>
+                  {val}
+                </HashTagStyle>
+              ))}
+          </HashTagsWrapper>
+          <div id="rating-star">
+            <RatingStar rating={rating} size="1rem" />
+            <LectureCommentCount>({commentCount})</LectureCommentCount>
+          </div>
+        </a>
+        <LectureShoppingContents>
+          <LecturePriceWrapper>
+            <LecturePrice price={price} discount={onDiscount} cardStyle="List" />
+          </LecturePriceWrapper>
+          <LectureCardIconBtnWrapper>
+            <IconButtons />
+          </LectureCardIconBtnWrapper>
+        </LectureShoppingContents>
+      </LectureCardContents>
+    </HorizonLectureCardStyle>
+  );
+};
+
+export default HorizonLectureCard;
---
+++ src/components/lectureCard/LecturePrice.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type PriceStyleProps = {
+  cardStyle: string;
+};
+
+const CurrentPrice = styled.span<PriceStyleProps>`
+  font-size: 1.125rem;
+  font-weight: 700;
+  margin-left: 0.25rem;
+  ${(props) => (props.cardStyle === 'Grid' ? 'color: var(--color-blue);' : '')}
+
+  @media screen and (max-width: 768px) {
+    font-size: ${(props) => (props.cardStyle === 'Grid' ? '0.9rem' : '1rem')};
+  }
+`;
+
+const OriginalPrice = styled.del<PriceStyleProps>`
+  ${(props) => (props.cardStyle === 'Grid' ? 'color: #595959;' : '')}
+  opacity: ${(props) => (props.cardStyle === 'Grid' ? '0.75' : '0.7')};
+  font-size: ${(props) => (props.cardStyle === 'Grid' ? '0.9rem' : '0.875rem')};
+  font-weight: ${(props) => (props.cardStyle === 'Grid' ? '500' : '700')};
+
+  @media screen and (max-width: 768px) {
+    ${(props) => (props.cardStyle === 'Grid' ? 'font-size: 0.85rem' : '')};
+  }
+`;
+type Props = {
+  price: number;
+  discount: number | undefined;
+  cardStyle: string;
+};
+
+const LecturePrice = ({ price, discount, cardStyle }: Props) => {
+  const discountPrice = discount ? Math.ceil(price * ((100 - discount) / 100)) : 0;
+
+  return (
+    <>
+      {discountPrice === 0 ? (
+        <CurrentPrice cardStyle={cardStyle}>
+          &#8361;{price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+        </CurrentPrice>
+      ) : (
+        <>
+          <OriginalPrice cardStyle={cardStyle}>
+            &#8361;{price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+          </OriginalPrice>
+          <CurrentPrice cardStyle={cardStyle}>
+            &#8361;{discountPrice.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+          </CurrentPrice>
+        </>
+      )}
+    </>
+  );
+};
+
+export default LecturePrice;
